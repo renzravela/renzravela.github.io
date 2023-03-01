f10e03b80a08914abe1baa1594eccefb6cb0eccb
renzravela <72610439+renzravela@users.noreply.github.com>
2023-03-01 23:27:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const body = document.querySelector('body')
+const toggle__btn = document.querySelector(".toggle__btn")
+const navbar = document.querySelector('.nav__links')
+const header = document.querySelector('header')
+
+
+toggle__btn.addEventListener("click", () => {
+    toggle__btn.classList.toggle("open__menu")
+    navbar.classList.toggle("open__navbar")
+
+    if(toggle__btn.classList.length == 2){
+        body.style.overflow = "hidden";
+    }else{
+        body.style.overflow = "auto";
+    }
+})
