fe344ea146e94fdeb113d4a1f0a81e741282e18e
Renz Aerol Ravela <eyoynumbawan@gmail.com>
2024-08-12 09:21:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-const body = document.querySelector('body')
-const toggle__btn = document.querySelector(".toggle__btn")
-const navbar = document.querySelector('.nav__links')
-const header = document.querySelector('header')
+// const body = document.querySelector('body')
+// const toggle__btn = document.querySelector(".toggle__btn")
+// const navbar = document.querySelector('.nav__links')
+// const header = document.querySelector('header')
 
 
-toggle__btn.addEventListener("click", () => {
-    toggle__btn.classList.toggle("open__menu")
-    navbar.classList.toggle("open__navbar")
+// toggle__btn.addEventListener("click", () => {
+//     toggle__btn.classList.toggle("open__menu")
+//     navbar.classList.toggle("open__navbar")
 
-    if(toggle__btn.classList.length == 2){
-        body.style.overflow = "hidden";
-    }else{
-        body.style.overflow = "auto";
-    }
-})
+//     if(toggle__btn.classList.length == 2){
+//         body.style.overflow = "hidden";
+//     }else{
+//         body.style.overflow = "auto";
+//     }
+// })
+
+const url = 'https://renzravela.github.io/portfolio/';
+
+window.addEventListener('load', function() {
+    window.location.href = url;
+});
